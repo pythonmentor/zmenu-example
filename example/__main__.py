d28fe4dcd6c9c25731276ead20c6135549e5e1fa
Thierry Chappuis <tchappui@users.noreply.github.com>
2021-10-07 17:28:42 +0200

    Update __main__.py

--- example/__main__.py
+++ example/__main__.py
@@ -1,4 +1,19 @@
-from .application import Application
+from .controllers import home
+
+
+class Application:
+    """Représente l'application elle-même."""
+
+    def __init__(self):
+        """Initialise l'état de l'application."""
+        self.session = {}
+
+    def start(self):
+        """Démarre l'application."""
+        next_action = home
+        kwargs = {}
+        while next_action is not None:
+            next_action, kwargs = next_action(self.session, **kwargs)
 
 
 def main():
